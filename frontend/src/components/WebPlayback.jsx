6b062564c86e8b972746bb93f9563b742324bf2d
Parshotan Seenanan <parshsee@gmail.com>
2023-12-18 21:06:12 -0500

    Added styling, removed search and context at load

--- frontend/src/components/WebPlayback.jsx
+++ frontend/src/components/WebPlayback.jsx
@@ -3,6 +3,7 @@ import SearchModal from './search/SearchModal';
 
 import DeviceContext from '../context/DeviceContext';
 import axios from 'axios';
+import { Button } from '@mui/material';
 
 const track = {
 	name: "",
@@ -117,16 +118,56 @@ function WebPlayback(props) {
 	if (!is_active) {
 		return (
 			<>
-			<DeviceContext.Provider value={{ deviceID: deviceID.current }}>
 				<div className="container">
 					<div className="main-wrapper">
 						<b> Instance not active. Transfer your playback using your Spotify app </b>
 					</div>
+				</div>
+			</>)
+	} else {
+		return (
+			<>
+				<DeviceContext.Provider value={{ deviceID: deviceID.current }}>
+					<img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
+
+					<div className="now-playing__name">{current_track.name}</div>
+					<div className="now-playing__artist">{current_track.artists[0].name}</div>
+
+					<Button variant="outlined" size='small' onClick={() => { player.previousTrack() }} >
+						&lt;&lt;
+					</Button>
+
+					<Button variant="outlined" size='small' onClick={() => { player.togglePlay() }} >
+						{is_paused ? "PLAY" : "PAUSE"}
+					</Button>
+
+					<Button variant="outlined" size='small' onClick={() => { player.nextTrack() }} >
+						&gt;&gt;
+					</Button>
+
+					<br />
+					<br />
 
 					<div>
-						<button onClick={() => { setShowSearchModal(true); }}>
+						<Button 
+							onClick={() => { setShowSearchModal(true); }}
+							variant='contained'
+						>
 							Search for Songs
-						</button>
+						</Button>
+					</div>
+
+					<div className='queued_songs_list'>
+						<h4>Queued Songs</h4>
+						{queue.map(track => {
+							return (
+								<span key={track.uri}>
+									<img src={track.album.images[1].url}/>
+									<div>{track.name}</div>
+									<div>{track.artists[0].name}</div>
+								</span>
+							)
+						})}
 					</div>
 
 					{showSearchModal && (
@@ -135,62 +176,6 @@ function WebPlayback(props) {
 							handleClose={handleCloseModals}
 						/>
 					)}
-				</div>
-			</DeviceContext.Provider>
-			</>)
-	} else {
-		return (
-			<>
-				<DeviceContext.Provider value={{ deviceID: deviceID.current }}>
-					<div className="container">
-						<div className="main-wrapper">
-
-							<img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
-
-							<div className="now-playing__side">
-								<div className="now-playing__name">{current_track.name}</div>
-								<div className="now-playing__artist">{current_track.artists[0].name}</div>
-
-								<button className="btn-spotify" onClick={() => { player.previousTrack() }} >
-									&lt;&lt;
-								</button>
-
-								<button className="btn-spotify" onClick={() => { player.togglePlay() }} >
-									{is_paused ? "PLAY" : "PAUSE"}
-								</button>
-
-								<button className="btn-spotify" onClick={() => { player.nextTrack() }} >
-									&gt;&gt;
-								</button>
-							</div>
-
-                            <div>
-								<button onClick={() => { setShowSearchModal(true); }}>
-									Search for Songs
-								</button>
-							</div>
-
-							<div className='queued_songs_list'>
-								<h4>Queued Songs</h4>
-								{queue.map(track => {
-									return (
-										<div key={track.uri}>
-											<img src={track.album.images[1].url}/>
-											<div className="queue__name">{track.name}</div>
-											<div className="queue__artist">{track.artists[0].name}</div>
-										</div>
-									)
-								})}
-							</div>
-
-							{showSearchModal && (
-								<SearchModal 
-									isOpen={showSearchModal}
-									handleClose={handleCloseModals}
-								/>
-							)}
-						</div>
-					</div>
 				</DeviceContext.Provider>
 			</>
 		);
